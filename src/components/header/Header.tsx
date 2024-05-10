8cbcb0faffe739d1e42952b5e0005f45c2959d19
James F. Thomas <jthomas@rsimail.com>
2024-05-10 10:35:37 -0500

    (UI) - styling Header with MUI makeStyles hook

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,15 +1,39 @@
-import { AppBar, IconButton, Toolbar, Typography } from "@material-ui/core";
+import { AppBar, Box, Toolbar, Typography } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
 
-import { Menu } from "@material-ui/icons";
 import ElevateOnScroll from "./parts/ElevationScroll";
+import { default as CompanyLogo } from "../../assets/logo.svg";
+
+const useStyles = makeStyles((theme) => ({
+  toolbarMargin: {
+    ...theme.mixins.toolbar,
+    marginBottom: "3em",
+  },
+  logo: {
+    height: "7em",
+  },
+}));
 
 const Header = () => {
+  const classes = useStyles();
+
   return (
-    <ElevateOnScroll>
-      <AppBar position="fixed" color="primary">
-        <Toolbar>Arch Development</Toolbar>
-      </AppBar>
-    </ElevateOnScroll>
+    <>
+      <ElevateOnScroll>
+        <AppBar position="fixed" color="primary">
+          <Toolbar disableGutters>
+            <img
+              src={CompanyLogo}
+              className={classes.logo}
+              alt="company logo"
+            />
+          </Toolbar>
+        </AppBar>
+      </ElevateOnScroll>
+      <Box // box acts as custom margin to prevent content from being hidden behind the app bar
+        className={classes.toolbarMargin}
+      />
+    </>
   );
 };
 
--- src/theme/customTheme.ts
+++ src/theme/customTheme.ts
@@ -1,16 +1,18 @@
 import { createTheme } from '@material-ui/core/styles';
+import { TypographyOptions } from '@material-ui/core/styles/createTypography';
 
 //custom color codes
 const archBlue = "#0B72B9";
 const archOrange = "#FFBA60";
 
-// this declaration is needed for the TS implementation of a MUI custom theme
+// this declaration is needed for the TS implementation of a MUI custom palette colors
 declare module '@material-ui/core/styles/createPalette' {
     interface CommonColors {
       blue?: string;
       orange?: string;
     }
   }
+
 
 const customTheme = createTheme({
     palette:{
@@ -24,7 +26,12 @@ const customTheme = createTheme({
         secondary:{
             main:`${archOrange}`
         }
-    }
+    },
+    typography:{
+        h3:{
+        fontWeight:300
+        }
+    },
 });
 
 export default customTheme;
